a8f47897a20b0e85f53320c9f357936966bf9377
Vadim <vad1nnden1sov@gmail.com>
2022-06-06 21:58:31 +0300

    slider created and adaptivated

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+      speed: 700,
+      //adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"></button>',
+      responsive: [
+        {
+            breakpoint: 920,
+            settings: {
+                arrows: false,
+                dots: true
+            }
+        }
+      ]
+  });
+});
